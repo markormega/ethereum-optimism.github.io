7ecd116fce18a342ccc4c7106c104d8249310a66
markormega <63770736+markormega@users.noreply.github.com>
2023-07-09 00:07:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Mark - Subdomain Takeover</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Mark - Subdomain Takeover" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          markanthonyormega@gmail.com</code>
         </p>
       </main>
 
